95b85441cedfe13aa10acb205cd22e96662df243
bassem-krayem <bassem.krayem09@gmail.com>
2025-10-13 09:27:59 +0100

    corrected the files  back to linux type anc changed them to be executable permissions

--- build-a-world-cup-database-project/insert_data.sh
+++ build-a-world-cup-database-project/insert_data.sh
@@ -1,10 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
 if [[ $1 == "test" ]]
 then
   PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
-  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+  PSQL="psql --username=bassem --dbname=worldcup -t --no-align -c"
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
